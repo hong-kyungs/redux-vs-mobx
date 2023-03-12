d0c7bbde79b01684fabe5b0a5101299f1f5891e2
hong-kyungs <hkyungs11@naver.com>
2023-03-12 16:23:50 +0900

    action 생성기

--- 1.redux/index.js
+++ 1.redux/index.js
@@ -8,5 +8,32 @@ const initialState = {
 };
 
 const store = createStore(reducer, initialState);
+store.subscribe(() => {
+	console.log('changed');
+});
 
-console.log(store.getState());
+console.log('1st', store.getState());
+
+// const changeCompA = {
+// 	type: 'CHANGE_COMP_A',
+// 	data: 'b'
+// }
+// const changeCompAtoC = {
+// 	type: 'CHANGE_COMP_A_TO_C',
+// 	data: 'c'
+// }
+// const changeCompAtoD = {
+// 	type: 'CHANGE_COMP_A_TO_D',
+// 	data: 'd'
+// }
+
+const changeCompA = (data) => {
+	return {
+		type: 'CHANGE_COMP_A',
+		data,
+	};
+};
+
+store.dispatch(changeCompA('B'));
+
+console.log('2nd', store.getState());
