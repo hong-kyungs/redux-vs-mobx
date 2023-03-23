a061e92b41b9491288eec6af52c4a7b294f43510
hong-kyungs <hkyungs11@naver.com>
2023-03-23 16:01:44 +0900

    createSelector 사용 예제

--- 5.redux-toolkit/App.jsx
+++ 5.redux-toolkit/App.jsx
@@ -1,3 +1,4 @@
+import { createSelector } from '@reduxjs/toolkit';
 import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -5,8 +6,17 @@ import { addPost } from './actions/post';
 import { logIn } from './actions/user';
 import  userSlice from './reducers/user';
 
+//useMemo의 역할을 createSelector로 한단계 더 끌어올림.
+//App 컴포넌트가 리렌더링되더라도 createSelector 부분에는 영향을 받지 않음.
+const priceSelector = (state) => state.user.prices;
+const sumPriceSelector = createSelector(
+  priceSelector,
+  (prices) =>  prices.reduce((a, c) => a + c, 0),
+);
+
 const App = () =>{
   const user = useSelector((state) => state.user); //state는 store의 initialState라고 생각하면 된다
+  const totalPrice = useSelector(sumPriceSelector);
   const dispatch = useDispatch();
 
   const [email, setEmail] = useState('');
@@ -36,7 +46,6 @@ const App = () =>{
     setPassword(e.target.value)
   }, [])
 
-  //
   const onSubmit = useCallback((e) => {
     e.preventDefault();
     dispatch(userSlice.actions.setLoginFrom({
@@ -45,6 +54,10 @@ const App = () =>{
     }))
   }, [dispatch, email, password])
   
+  // const totalPrice = useMemo(() => {
+  //   console.log('memo');
+  //   return prices.reduce((a, c) => a + c, 0);
+  // }, [prices]);
 
   return(
     <div>
@@ -59,7 +72,10 @@ const App = () =>{
       {user.data && <button onClick={onLogout}>로그아웃</button>}
       <button onClick={onAddPost}>게시글 작성</button>
 
-      {/* form예제처럼 input에서는 redux를 사용하지 않고 input에 state를 썼다가 한번에 모아서 form에서 redux로 내보내는 것이 좋다.*/}
+      {/* 임의의 숫자들을 만들고 더하는 예제 */}
+      <div><b>{totalPrice}원</b></div>
+
+      {/* form예제처럼 한 컴포넌트안에서만 쓰이는 것들,input에서는 dispatch로 reducer로 보내지말고, input에 state를 썼다가 한번에 모아서 form에서 redux로 내보내는 것이 좋다.*/}
       <form onSubmit={onSubmit}>
         <input type="email" value={email} onChange={onChangeEmail}/>
         <input type="password" value={password} onChange={onChanagePassword}/>
